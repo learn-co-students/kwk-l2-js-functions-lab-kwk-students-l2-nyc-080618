32af9e7464ba1d54657446941d2bbf1a9f2da1db
Annie Wang <anwang911@gmail.com>
2018-08-08 18:55:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(street){
+ var results = street -42;
+ if (results < 0){
+   return results * -1;
+ }else{
+   return results;
+ }
+}
+
+distanceFromHqInBlocks(street);
+
+function distanceFromHqInFeet(block){
+ return (distanceFromHqInBlocks(block)* 264);
+}
+
+distanceFromHqInFeet(block);
+
+function distanceTravelledInFeet(start, end) {
+ var results = ((end-start)*264);
+ if (results < 0){
+   return results * -1;
+ }else{
+   return results;
+ }
+}
+
+distanceTravelledInFeet(start, end);
+
+function calculatesFarePrice(start, destination) {
+ var feetTravelled = distanceTravelledInFeet(start, destination);
+ if (feetTravelled > 400 && feetTravelled <= 2000) {
+   return 0.02 * (feetTravelled-400);
+ } else if (feetTravelled > 2000 && feetTravelled <=2500) {
+   return 25;
+ } else if (feetTravelled > 2500) {
+   return "cannot travel that far";
+ } else {
+   return 0;
+ }
+ 
+}
